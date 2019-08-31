31e63ca16d8530048a47e328e69821d66b54ee94
Darren Hsu <darren.hsu@chinalife.com.tw>
2019-08-31 18:26:18 +0800

    update code

--- DHSecurity/AppDelegate.swift
+++ DHSecurity/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        #if !(arch(i386) || arch(x86_64))
+        if UIApplication.isJailbreak() {
+            print("is jb")
+        }else {
+            print("not jb")
+        }
+        #endif
+        
         return true
     }
 
---
+++ DHSecurity/Class/Extension/UIApplication+Jailbreak.swift
@@ -0,0 +1,82 @@
+//
+//  UIAppDelegate+Jailbreak.swift
+//  DHAssesmentCollection
+//
+//  Created by wen on 2019/8/31.
+//  Copyright © 2019 D.H. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+    public static func isJailbreak() -> Bool {
+        guard let cydiaUrlScheme = NSURL(string: "cydia://package/com.example.package") else { return false }
+        if UIApplication.shared.canOpenURL(cydiaUrlScheme as URL) {
+            return true
+        }
+        
+        let paths: [String] = ["/Applications/Cydia.app",
+                               "/Applications/FakeCarrier.app",
+                               "/Applications/Icy.app",
+                               "/Applications/IntelliScreen.app",
+                               "/Applications/MxTube.app",
+                               "/Applications/RockApp.app",
+                               "/Applications/SBSettings.app",
+                               "/Applications/WinterBoard.app",
+                               "/Applications/blackra1n.app",
+                               "/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+                               "/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+                               "/Library/MobileSubstrate/MobileSubstrate.dylib",
+                               "/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+                               "/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+                               "/bin/bash",
+                               "/bin/sh",
+                               "/etc/apt",
+                               "/etc/ssh/sshd_config",
+                               "/private/var/lib/apt",
+                               "/private/var/lib/cydia",
+                               "/private/var/mobile/Library/SBSettings/Themes",
+                               "/private/var/stash",
+                               "/private/var/tmp/cydia.log",
+                               "/usr/bin/sshd",
+                               "/usr/libexec/sftp-server",
+                               "/usr/libexec/ssh-keysign",
+                               "/usr/sbin/sshd",
+                               "/var/cache/apt",
+                               "/var/lib/apt",
+                               "/var/lib/cydia",
+                               "/usr/sbin/frida-server",
+                               "/usr/bin/cycript",
+                               "/usr/local/bin/cycript",
+                               "/usr/lib/libcycript.dylib"]
+        
+        let fileManager = FileManager.default
+        for path in paths {
+            if fileManager.fileExists(atPath: path) {
+                debugPrint("\(path) file exists")
+                return true
+            }
+            
+            if self.canOpen(path: path) {
+                debugPrint("\(path) can open")
+                return true
+            }
+        }
+        
+        let path = "/private/" + NSUUID().uuidString
+        do {
+            try "anyString".write(toFile: path, atomically: true, encoding: String.Encoding.utf8)
+            try fileManager.removeItem(atPath: path)
+            return true
+        } catch {}
+        
+        return false
+    }
+    
+    private static func canOpen(path: String) -> Bool {
+        let file = fopen(path, "r")
+        guard file != nil else { return false }
+        fclose(file)
+        return true
+    }
+}
